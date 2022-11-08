4f96a43ea4d63f80880f9e2ad18f59d7393d539f
Hai Long <hailongkc1203@gmail.com>
2022-11-08 19:41:16 +0700

    bai24

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,14 +2,31 @@ import React from "react";
 
 class Displayinfor extends React.Component {
     render() {
+        console.log();
         // destructuring array/object
-        const { age, name } = this.props;
+        const { listUsers } = this.props;
+        console.log(listUsers);
         // props => viet tat cua properties
         console.log(this.props);
         return (
             <div>
+                {listUsers.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <div>My Name is {user.name}</div>
+                            <div>My age's {user.age}</div>
+                            <hr />
+                        </div>
+                    )
+                })}
+                {/* <div>My Name is {name}</div>
+                <div>My age {age}</div>
+                <hr />
                 <div>My Name is {name}</div>
                 <div>My age {age}</div>
+                <hr />
+                <div>My Name is {name}</div>
+                <div>My age {age}</div> */}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,23 @@ import Displayinfor from "./DisplayInfor";
 import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
-
+    state = {
+        listUsers: [
+            { id: 1, name: "Hai", age: "21" },
+            { id: 2, name: "Long", age: "22" },
+            { id: 3, name: "Hai Long", age: "25" },
+        ]
+    }
     // JSX
     render() {
         return (
             < div >
                 <UserInfor></UserInfor>
                 <br /> <br />
-                <Displayinfor name="Hai Long" age="21"></Displayinfor>
+                <Displayinfor
+                    listUsers={this.state.listUsers}
+                />
+                <hr />
             </div>
         );
     }
