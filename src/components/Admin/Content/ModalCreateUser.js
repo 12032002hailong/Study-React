f5aff6b34559730f0841d142a2b8ded52815e306
Hai Long <hailongkc1203@gmail.com>
2022-11-13 23:48:31 +0700

    customize axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
-import axios from 'axios';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from "../../../service/apiServices"
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -57,22 +57,13 @@ const ModalCreateUser = (props) => {
             return;
         }
 
-        //submit dât
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('usename', usename);
-        data.append('role', role);
-        data.append('useImage', image);
-
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log(">>> check res :", res.data);
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        let data = await postCreateNewUser(email, password, usename, role, image)
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
-        if (res.data && res.data.Ec !== 0) {
-            toast.error(res.data.EM);
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
         }
     }
 
---
+++ src/service/apiServices.js
@@ -0,0 +1,15 @@
+import axios from '../utils/axiosCustomize';
+
+const postCreateNewUser = (email, password, usename, role, image) => {
+    //submit dât
+    const data = new FormData();
+    data.append('email', email);
+    data.append('password', password);
+    data.append('usename', usename);
+    data.append('role', role);
+    data.append('useImage', image);
+
+    return axios.post('api/v1/participant', data);
+}
+
+export { postCreateNewUser };
