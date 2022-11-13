182777144be813e6caa8fc1cb0d915fd082831a8
Hai Long <hailongkc1203@gmail.com>
2022-11-14 00:53:48 +0700

    Cập nhật danh sách người dùng

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -3,10 +3,26 @@ import './ManageUser.scss';
 import { FcPlus } from 'react-icons/fc';
 import { useState } from 'react';
 import TableUser from "./TableUser.js";
-
+import { useEffect } from 'react';
+import { getAllUsers } from '../../../service/apiServices';
 
 const ManageUser = () => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+
+    const [listUsers, setListUsers] = useState([
+
+    ]);
+
+    useEffect(async () => {
+        fetchListUsers();
+    }, []);
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
     return (
         <div className="manage-user-container">
             <div className="title">
@@ -19,11 +35,12 @@ const ManageUser = () => {
                         <FcPlus />Add new users</button>
                 </div>
                 <div className="table-users-container">
-                    <TableUser />
+                    <TableUser listUsers={listUsers} />
                 </div>
                 <ModalCreateUser
                     show={showModalCreateUser}
                     setShow={setShowModalCreateUser}
+                    fetchListUsers={fetchListUsers}
                 />
             </div>
         </div>
