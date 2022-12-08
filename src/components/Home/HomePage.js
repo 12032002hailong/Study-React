91b1b43fb4de2795f12cbf5a788f980b9adda627
Hai Long <hailongkc1203@gmail.com>
2022-12-08 17:40:33 +0700

    Design Danh Sach Bai Thi

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,9 @@
 import videoHomePage from '../../assets/video-homepage.mp4'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const HomePage = (props) => {
     const isAuthenticated = useSelector(state => state.user.isAuthenticated);
-    const account = useSelector(state => state.user.account);
+    const navigate = useNavigate();
     return (
         <div className="homepage-container">
             <video autoPlay muted loop>
@@ -15,7 +16,12 @@ const HomePage = (props) => {
                 <div className='title-1'>There's a better way to ask</div>
                 <div className='title-2'>You don't want to make a boring form. And your audience won't answer one. Create a typeform instead—and make everyone happy.</div>
                 <div className='title-3'>
-                    <button>Get's started. It's free</button>
+                    {isAuthenticated === false ?
+                        <button onClick={() => navigate('/login')}>Get's started. It's free</button>
+                        :
+                        <button onClick={() => navigate('/users')}>Dping Quiz Now</button>
+                    }
+
                 </div>
             </div>
         </div>
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -48,9 +48,13 @@ const postRegister = (email, password, username) => {
     return axios.post(`/api/v1/register`, { email, password, username });
 }
 
+const getQuizByUser = () => {
+    return axios.get('/api/v1/quiz-by-participant');
+}
+
 export {
     postCreateNewUser, getAllUsers,
     putUpdateUser, deleteUser,
     getUserWithPaginate, postLogin,
-    postRegister
+    postRegister, getQuizByUser
 };
